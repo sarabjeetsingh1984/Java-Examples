c0b3c562219dd094bd4c27b7a1d9ca5d4ac918f8
Sarabjeet <Sarabjeet.X.Singh.-ND@disney.com>
2016-04-25 12:18:47 +0530

    First commit

---
+++ src/com/java/examples/HashMap/HashMapStructure.java
@@ -0,0 +1,53 @@
+package com.java.examples.HashMap;
+
+import java.util.HashMap;
+
+import java.util.Iterator;
+
+public class HashMapStructure {
+
+    /**
+
+     * @author Arpit Mandliya
+
+     */
+
+    public static void main(String[] args) {
+
+        Country india=new Country("India",1000);
+
+        Country japan=new Country("India",1000);
+
+        Country france=new Country("France",2000);
+
+        Country russia=new Country("Russia",20000);
+
+        HashMap<Country,String> countryCapitalMap=new HashMap<Country,String>();
+
+        countryCapitalMap.put(india,"Delhi");
+
+        countryCapitalMap.put(japan,"Tokyo");
+
+        countryCapitalMap.put(france,"Paris");
+
+        countryCapitalMap.put(russia,"Moscow");
+
+        Iterator<Country> countryCapitalIter=countryCapitalMap.keySet().iterator();//put debug point at this line
+
+        while(countryCapitalIter.hasNext())
+
+        {
+
+            Country countryObj=countryCapitalIter.next();
+
+            String capital=countryCapitalMap.get(countryObj);
+
+            System.out.println(countryObj.getName()+"----"+capital + "\n");
+            
+            System.out.println("Hash Code : "+"----"+countryObj.hashCode());
+
+            }
+
+        }
+
+}
